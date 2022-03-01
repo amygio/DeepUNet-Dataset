17ae0928a2b7e85d91c97bf15cd3362f2a18ef2d
amygio <97981865+amygio@users.noreply.github.com>
2022-03-01 13:23:18 +0100

    Update module.py

--- module.py
+++ module.py
@@ -21,7 +21,7 @@ class Dataset:
             height = shape[0]
             width = shape[1]
             #Seleziono in maniera random dei punti in un certo range calcolato in modo tale che si possa fare il crop senza rischiare di uscire dall'immagine 
-            for i in range (0,3):
+            for i in range (0,3): #modificare opportunamente il range
                 x = random.randrange(0, width - 256)
                 y = random.randrange(0, height - 256)
                 # crop size 256x256
